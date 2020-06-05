004da8c12f5d1981b6f666e338027ec71d4b7831
JulayJuliane <srmalcher@gmail.com>
2020-06-05 11:21:26 -0300

    files

---
+++ scripts/intro.js
@@ -0,0 +1,67 @@
+// coments
+
+// documents.write(Hello)
+
+// variaveis, tipos de dados
+// let myvar = "He"
+// const myconst = "He"
+// document.write (myconst + myvar)
+
+
+// string
+// "Isso é uma string"
+// 'Isso também é uma string'
+// `Isso é uma string também`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write (n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidade
+// const pessoa = {
+//    altura: "1,80",
+//    idade: 24,
+//    solteiro: true
+//    correr(){
+//      document.write("exeecutar uma grande logica de correr")
+//    }
+// }
+// document.correr ()
+
+
+// Array - Vetores = coleção de algo
+// [....]
+// const colecaoDeBolinhas = ["blue", "green", 1, {name:"My Nanme"}]
+// document.write(colecaoDeBolinhas[0].name)
+// -> vai escrever na tela blue e My Name
+
+
+// Funções e funcionalidades 
+// function sayMyName (name) {
+//    documnet.write(name)
+// }
+// executar
+// sayMyName("Douglas")
+// sayMyName("Valeska")
+// sayMyName("Kyam")
+
+
+// condicionais
+//const notaFinal = 4
+
+//if ( notaFinal < 5 ) {
+//  document.write ("Reprovado")
+//} else {
+//  document.write ("Aprovado")
+//} 
+
+// lopp repetições
+// for ( i = 0; i <10; i++) {
+//   document.write(`<p>${i} </p>`)
+// }
