6a19959c6f7ae6c54dbd5bc04ba8389e5d9f4506
JulayJuliane <srmalcher@gmail.com>
2020-06-05 16:33:24 -0300

    mudei

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,9 +1,11 @@
 
 function populateUFs(){
     const ufSelect = document.querySelector("select[name=uf]")
+    
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
     .then( res => res.json() )
-    .then( states => {
+    .then( states => { 
+        
 
         for( const state of states ){
             ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`    
@@ -16,8 +18,8 @@ populateUFs()
 
 
 function getCities(event){
-    const  citySelect = document.querySelector("select[name=city]")
-    const  stateInput = document.querySelector("select[name=state]")
+    const  citySelect = document.querySelector("[name=city]")
+    const  stateInput = document.querySelector("[name=state]")
     
     const ufValue = event.target.value
 
@@ -25,12 +27,17 @@ function getCities(event){
     stateInput.value = event.target.options[indexOfSelectedState].text
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+    
+
+    citySelect.innerHTML = ""
+    citySelect.disabled = true
+
     fetch(url)
     .then( res => res.json() )
     .then( cities => {
-
+        
        for( const city of cities ){
-           citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`    
+           citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`    
        }
        
        citySelect.disabled = false
@@ -41,3 +48,43 @@ function getCities(event){
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+
+// itens de coleta
+// pegar todos os li's
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect){
+    item.addEventListener("click", handleSelectedItem)
+}
+
+let selectedItems = [1,2,3,4,5,6]
+
+
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+    
+    //adicionar ou remover uma classe com javascript
+    itemLi.classList.toggle("selected")
+    const itemId = itemLi.dataset.id 
+
+    
+
+    // verificar se existem itens selecionados, 
+    // se sim, pegar os items selecionados
+    const alreadySelected = selectedItems.findIndex(function() { const itemFound = item == itemId //isso será true ou falso
+        return itemFound
+    } )
+
+    console.log(alreadySelected)
+    
+    // se já tiver selecionado, tirar da seleção 
+    if
+
+
+    // se  não estiver selecionado, adicionar a seleção
+
+
+    // atualizar o campo escondido com os itens selecionados
+}
--- scripts/intro.js
+++ scripts/intro.js
@@ -1,29 +1,34 @@
-// coments
+// COMENTS
 
 // documents.write(Hello)
 
-// variaveis, tipos de dados
+
+
+// VARIAVEIS  E TIPOS DE DADOS 
 // let myvar = "He"
 // const myconst = "He"
 // document.write (myconst + myvar)
 
 
-// string
+// STRING
 // "Isso é uma string"
 // 'Isso também é uma string'
 // `Isso é uma string também`
 
-// number
+
+// CONSTANTES COM NUMEROS
 // const n1 = 1
 // const n2 = 12
 // document.write (n1 + n2)
 
-// boolean - true ou false
+
+// BOOLEAN - true ou false
 // const bTrue = true
 // const bFalse = false
 // document.write(bFalse)
 
-// objeto possuem propriedades e funcionalidade
+
+// OBJETO possuem propriedades e funcionalidade
 // const pessoa = {
 //    altura: "1,80",
 //    idade: 24,
@@ -35,14 +40,14 @@
 // document.correr ()
 
 
-// Array - Vetores = coleção de algo
+// ARRAY - VETORES = coleção de algo
 // [....]
 // const colecaoDeBolinhas = ["blue", "green", 1, {name:"My Nanme"}]
 // document.write(colecaoDeBolinhas[0].name)
 // -> vai escrever na tela blue e My Name
 
 
-// Funções e funcionalidades 
+// FUNÇÕES e funcionalidades 
 // function sayMyName (name) {
 //    documnet.write(name)
 // }
@@ -52,7 +57,7 @@
 // sayMyName("Kyam")
 
 
-// condicionais
+// CONDICIONAIS
 //const notaFinal = 4
 
 //if ( notaFinal < 5 ) {
@@ -61,7 +66,9 @@
 //  document.write ("Aprovado")
 //} 
 
-// lopp repetições
+
+
+// LOOP = repetições
 // for ( i = 0; i <10; i++) {
 //   document.write(`<p>${i} </p>`)
 // }
