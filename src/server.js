6f44cd9a9ce7f7a9c3b637c387abfb922a74871c
JulayJuliane <srmalcher@gmail.com>
2020-06-06 21:49:05 -0300

    change layout

---
+++ src/server.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const server = express()
+
+//configurar pasta publica
+server.use(express.static("public"))
+
+
+
+// utilizando template engine
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+// configurar caminhos da minha aplicação
+
+// página inicial
+// req = Requisição
+// res = Resposta
+server.get("/", (req,res) => {
+  return res.render("index.html", { title: "Um título"}) 
+})
+
+// página create-point
+server.get("/create-point", (req,res) => {
+  return res.render("create-point.html") 
+})
+
+// página create-point
+server.get("/search", (req,res) => {
+   return res.render("search-results.html") 
+})
+
+//ligar o servidor
+server.listen(3000)
